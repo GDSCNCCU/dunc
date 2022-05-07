80d066b04441751536bab1280d891661830bf6ab
109703010 <andyszztzn@gmail.com>
2022-05-07 17:40:14 +0800

    Search page update

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
   runApp(const MyApp());
 }
 
-double? screenWidth;  // set in build of _MyHomePageState
+double? screenWidth; // set in build of _MyHomePageState
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -37,7 +37,13 @@ class SearchView extends StatefulWidget {
   State<SearchView> createState() => _SearchViewState();
 }
 
+int selectedIndex = 0;
+
 class _SearchViewState extends State<SearchView> {
+  NeumorphicTextStyle toggleTextStyle = NeumorphicTextStyle(
+      fontWeight: FontWeight.bold,
+      fontFamily: "GenSenRounded JP",
+      fontSize: 16);
 
   @override
   Widget build(BuildContext context) {
@@ -46,189 +52,148 @@ class _SearchViewState extends State<SearchView> {
       backgroundColor: backgroundColor,
       body: Column(
         children: [
-          Neumorphic(  // 「找球隊/球員、找比賽」
-            margin: const EdgeInsets.only(
-                left: 50,
-                top: 50,
-                right: 50,
-                bottom: 12
-            ),
-            style: NeumorphicStyle(
-              color: Colors.white,
-              shape: NeumorphicShape.flat,
-              depth: -3,
-              intensity: 1,
-              surfaceIntensity: 0,
-              boxShape: NeumorphicBoxShape.roundRect(const BorderRadius.all(Radius.circular(20))),
-            ),
-            child: NeumorphicToggle(
-              children: [
-                ToggleElement(
-                  foreground: Center(child: NeumorphicText(
-                    '找球隊/球員',
-                    style: const NeumorphicStyle(color: Colors.white),
-                    textStyle: NeumorphicTextStyle(
-                      fontFamily: "GenSenRounded JP",
-                      fontSize: 16
-                    ),
-                  ),),
-                  background: Center(child: NeumorphicText(
-                    '找球隊/球員',
-                    style: const NeumorphicStyle(color: Colors.black),
-                      textStyle: NeumorphicTextStyle(
-                        fontFamily: "GenSenRounded JP",
-                        fontSize: 16
-                      )
-                  ),),
-                ),
-                ToggleElement(
-                  foreground: Center(child: NeumorphicText(
-                    '找比賽',
-                    style: const NeumorphicStyle(color: Colors.white),
-                      textStyle: NeumorphicTextStyle(
-                        fontFamily: "GenSenRounded JP",
-                        fontSize: 16
-                      )
-                  ),),
-                  background: Center(child: NeumorphicText(
-                    '找比賽',
-                    style: const NeumorphicStyle(color: Colors.black),
-                      textStyle: NeumorphicTextStyle(
-                        fontFamily: "GenSenRounded JP",
-                        fontSize: 16
-                      )
-                  ),)
-                )
-              ],
-
-              thumb: Container(  // 已被選擇的項目的樣式
-                foregroundDecoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.bottomLeft,
-                    end: Alignment.topRight,
-                    colors: [blue, purple]
-                  ),
-                ),
+          Neumorphic(
+              // 「找球隊/球員、找比賽」
+              margin: const EdgeInsets.only(
+                  left: 60, top: 50, right: 60, bottom: 12),
+              style: NeumorphicStyle(
+                color: Colors.white,
+                depth: -3,
+                intensity: 1,
+                surfaceIntensity: 0,
+                boxShape: NeumorphicBoxShape.roundRect(
+                    const BorderRadius.all(Radius.circular(20))),
               ),
-
-              style: const NeumorphicToggleStyle(  // 沒被選擇的項目的樣式
-                backgroundColor: backgroundColor,
-                lightSource: LightSource(-0.6, -0.6),
-                borderRadius: BorderRadius.all(Radius.circular(20)),  // 選中項目
-              ),
-            )
-          ),
-          Neumorphic(  // 搜尋
-            margin: const EdgeInsets.only(
-              left: 50,
-              top: 12,
-              right: 50,
-              bottom: 12
-            ),
-            style: NeumorphicStyle(
-              color: backgroundColor,
-              shape: NeumorphicShape.flat,
-              depth: -3,
-              intensity: 1,
-              surfaceIntensity: 0,
-              boxShape: NeumorphicBoxShape.roundRect(const BorderRadius.all(Radius.circular(20))),
-            ),
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              child: Row(
+              child: NeumorphicToggle(
+                movingCurve: Curves.easeInBack,
+                duration: const Duration(milliseconds: 500),
+                selectedIndex: selectedIndex,
+                onChanged: (int index) {
+                  setState(() {
+                    selectedIndex = index;
+                  });
+                },
                 children: [
-                  const SizedBox(
-                    width: 35,
-                    child: Icon(
-                      Icons.search,
-                      color: Colors.grey,
+                  ToggleElement(
+                    foreground: Center(
+                        child: NeumorphicText('找球隊/球員',
+                            style: const NeumorphicStyle(
+                                color: Colors.white, depth: 0),
+                            textStyle: toggleTextStyle)),
+                    background: Center(
+                      child: NeumorphicText('找球隊/球員',
+                          style: const NeumorphicStyle(
+                              color: Colors.grey, depth: 0),
+                          textStyle: toggleTextStyle),
                     ),
                   ),
-                  SizedBox(
-                    width: screenWidth! - 155,  // 扣掉margin、搜尋icon
-                    child: TextField(
-                      decoration: null,
-                      keyboardType: TextInputType.name,
-                      textInputAction: TextInputAction.search,
-                      autofocus: true,
-                      cursorColor: blue,
-                      style: const TextStyle(
-                        fontFamily: "GenSenRounded JP",
-                        fontSize: 14
+                  ToggleElement(
+                      foreground: Center(
+                        child: NeumorphicText('找比賽',
+                            style: const NeumorphicStyle(
+                                color: Colors.white, depth: 0),
+                            textStyle: toggleTextStyle),
                       ),
-                      onChanged: (str) {}, //todo
-                    ),
-                  )
+                      background: Center(
+                        child: NeumorphicText('找比賽',
+                            style: const NeumorphicStyle(
+                                color: Colors.grey, depth: 0),
+                            textStyle: toggleTextStyle),
+                      ))
                 ],
+                thumb: Container(
+                  // 已被選擇的項目的樣式
+                  foregroundDecoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.bottomLeft,
+                        end: Alignment.topRight,
+                        colors: [blue, purple]),
+                  ),
+                ),
+                style: const NeumorphicToggleStyle(
+                  // 沒被選擇的項目的樣式
+                  backgroundColor: backgroundColor,
+                  lightSource: LightSource(-0.6, -0.6),
+                  borderRadius: BorderRadius.all(Radius.circular(20)), // 選中項目
+                ),
+              )),
+          Neumorphic(
+              // 搜尋
+              margin: const EdgeInsets.only(
+                  left: 50, top: 12, right: 50, bottom: 12),
+              style: NeumorphicStyle(
+                color: backgroundColor,
+                depth: -3,
+                intensity: 1,
+                surfaceIntensity: 0,
+                boxShape: NeumorphicBoxShape.roundRect(
+                    const BorderRadius.all(Radius.circular(40))),
               ),
-            ),
-          ),
-          Row(  // 搜尋過、已加入我的最愛
+              child: SizedBox(
+                width: screenWidth! - 100, // 扣掉margin、搜尋icon
+                child: TextField(
+                  decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintStyle: TextStyle(color: Colors.white),
+                      hintText: "輸入球隊/員",
+                      prefixIcon: Icon(
+                        Icons.search,
+                        size: 25,
+                        color: Colors.grey,
+                      )),
+                  keyboardType: TextInputType.name,
+                  textInputAction: TextInputAction.search,
+                  autofocus: true,
+                  cursorColor: blue,
+                  style: const TextStyle(
+                      fontFamily: "GenSenRounded JP", fontSize: 20),
+                  onChanged: (str) {}, //todo
+                ),
+              )),
+          Row(
+            // 搜尋過、已加入我的最愛
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Spacer(flex: 2,),
-              NeumorphicButton(  // 搜尋過
-                margin: const EdgeInsets.only(
-                  left: 0,
-                  top: 15,
-                  right: 0,
-                  bottom: 12
-                ),
-                padding: const EdgeInsets.only(
-                  top: 3,
-                  bottom: 3,
-                  left: 20,
-                  right: 20
-                ),
-                style: NeumorphicStyle(
-                  depth: 0,
-                  color: const Color(0xffbb9bcc),
-                  disableDepth: true,
-                  boxShape: NeumorphicBoxShape.roundRect(const BorderRadius.all(Radius.circular(16)))
-                ),
-                child: NeumorphicText(
-                  "搜尋過",
-                  style: const NeumorphicStyle(
-                    color: Colors.white
-                  ),
-                  textStyle: NeumorphicTextStyle(
-                    fontFamily: "GenSenRounded JP",
-                    fontSize: 12
-                  )
-                ),
+              const Spacer(
+                flex: 2,
               ),
-              const Spacer(),
               NeumorphicButton(
+                // 搜尋過
                 margin: const EdgeInsets.only(
-                    left: 0,
-                    top: 15,
-                    right: 0,
-                    bottom: 12
-                ),
+                    left: 0, top: 15, right: 0, bottom: 12),
                 padding: const EdgeInsets.only(
-                top: 3,
-                bottom: 3,
-                left: 11,
-                right: 11
-                ),
+                    top: 3, bottom: 3, left: 20, right: 20),
                 style: NeumorphicStyle(
                     depth: 0,
                     color: const Color(0xffbb9bcc),
                     disableDepth: true,
-                    boxShape: NeumorphicBoxShape.roundRect(const BorderRadius.all(Radius.circular(16)))
-                ),
-                child: NeumorphicText(
-                  "已加入我的最愛",
-                  style: const NeumorphicStyle(
-                      color: Colors.white
-                  ),
-                  textStyle: NeumorphicTextStyle(
-                    fontFamily: "GenSenRounded JP",
-                    fontSize: 12
-                  )
-                ),
+                    boxShape: NeumorphicBoxShape.roundRect(
+                        const BorderRadius.all(Radius.circular(16)))),
+                child: NeumorphicText("搜尋過",
+                    style: const NeumorphicStyle(color: Colors.white),
+                    textStyle: NeumorphicTextStyle(
+                        fontFamily: "GenSenRounded JP", fontSize: 12)),
               ),
-              const Spacer(flex: 2,)
+              const Spacer(),
+              NeumorphicButton(
+                margin: const EdgeInsets.only(
+                    left: 0, top: 15, right: 0, bottom: 12),
+                padding: const EdgeInsets.only(
+                    top: 3, bottom: 3, left: 11, right: 11),
+                style: NeumorphicStyle(
+                    depth: 0,
+                    color: const Color(0xffbb9bcc),
+                    disableDepth: true,
+                    boxShape: NeumorphicBoxShape.roundRect(
+                        const BorderRadius.all(Radius.circular(16)))),
+                child: NeumorphicText("已加入我的最愛",
+                    style: const NeumorphicStyle(color: Colors.white),
+                    textStyle: NeumorphicTextStyle(
+                        fontFamily: "GenSenRounded JP", fontSize: 12)),
+              ),
+              const Spacer(
+                flex: 2,
+              )
             ],
           )
         ],
