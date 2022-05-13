31d126c4ba4401368d1af4a77f6be5fde7e62afc
timsu92 <33785401+timsu92@users.noreply.github.com>
2022-05-13 23:14:38 +0800

    搜尋頁：移除多餘widget
    新版Figma沒有這個東西

--- lib/search/team.dart
+++ lib/search/team.dart
@@ -55,51 +55,5 @@ List<Widget> teamWidgets(
             }, //todo: unknown, waiting for UI
           ),
         )),
-    Row(
-      // 搜尋過、已加入我的最愛
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Spacer(
-          flex: 2,
-        ),
-        NeumorphicButton(
-          // 搜尋過
-          margin: const EdgeInsets.only(
-              left: 0, top: 15, right: 0, bottom: 12),
-          padding: const EdgeInsets.only(
-              top: 3, bottom: 3, left: 20, right: 20),
-          style: NeumorphicStyle(
-              depth: 0,
-              color: const Color(0xffbb9bcc),
-              disableDepth: true,
-              boxShape: NeumorphicBoxShape.roundRect(
-                  const BorderRadius.all(Radius.circular(16)))),
-          child: NeumorphicText("搜尋過",
-              style: const NeumorphicStyle(color: Colors.white),
-              textStyle: NeumorphicTextStyle(
-                  fontFamily: "GenSenRounded JP", fontSize: 12)),
-        ),
-        const Spacer(),
-        NeumorphicButton(
-          margin: const EdgeInsets.only(
-              left: 0, top: 15, right: 0, bottom: 12),
-          padding: const EdgeInsets.only(
-              top: 3, bottom: 3, left: 11, right: 11),
-          style: NeumorphicStyle(
-              depth: 0,
-              color: const Color(0xffbb9bcc),
-              disableDepth: true,
-              boxShape: NeumorphicBoxShape.roundRect(
-                  const BorderRadius.all(Radius.circular(16)))),
-          child: NeumorphicText("已加入我的最愛",
-              style: const NeumorphicStyle(color: Colors.white),
-              textStyle: NeumorphicTextStyle(
-                  fontFamily: "GenSenRounded JP", fontSize: 12)),
-        ),
-        const Spacer(
-          flex: 2,
-        )
-      ],
-    )
   ];
 }
