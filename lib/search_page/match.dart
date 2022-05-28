505854fdf24ec3fa192825cc38546b364498e6e4
timsu92 <33785401+timsu92@users.noreply.github.com>
2022-05-28 15:59:58 +0800

    搜尋頁
    - 將找球隊與找比賽都從函數回傳改為Stateless/Stateful Widget
    - 將找球隊的文字輸入框控制器改成全域變數，讓他可以在改變頁面時不會遺失狀態

--- lib/search_page/match.dart
+++ lib/search_page/match.dart
@@ -1,6 +1,11 @@
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import '../tools/colors.dart';
 
-List<Widget> matchWidgets(){
-  return <Widget>[]; //todo
+class MatchWidgets extends StatelessWidget {
+  const MatchWidgets({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(); // todo
+  }
 }
