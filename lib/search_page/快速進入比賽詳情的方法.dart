89cd082c2faaa452f9d1bb445ef09d97cbd6cdf1
timsu92 <33785401+timsu92@users.noreply.github.com>
2022-10-15 11:42:30 +0800

    快速進入match_info的方法

---
+++ lib/search_page/快速進入比賽詳情的方法.dart
@@ -0,0 +1,18 @@
+
+import './match_info.dart';
+      // body: Center(
+      //     child: NeumorphicButton(
+      //       style: const NeumorphicStyle(
+      //           color: Colors.yellow,
+      //           depth: 5
+      //       ),
+      //       onPressed: (){
+      //         Navigator.push(context, MaterialPageRoute(
+      //             settings: const RouteSettings(
+      //                 arguments: 'example'
+      //             ),
+      //             builder: (context) => const MatchInfo()
+      //         ));
+      //       },
+      //     )
+      // ),
